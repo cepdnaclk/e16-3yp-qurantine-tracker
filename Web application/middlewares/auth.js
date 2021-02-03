dffcc2ecb3832db298b77f6be7ee86ff58a51cf5
Tharushi <63853025+TharushiSuwaris@users.noreply.github.com>
2021-02-03 09:44:03 +0530

    Update auth.js

--- Web application/middlewares/auth.js
+++ Web application/middlewares/auth.js
@@ -15,7 +15,46 @@ const auth = async(req, res, next)=>{
          // cookie not found redirect to login 
          return res.redirect('/login');
       }
-   }catch (e) {
+   }catch (e) const mysqlConnection = require("../db/mysql_connection");
+var jwt = require("jsonwebtoken");
+
+const auth = async (req, res, next) => {
+  try {
+    let token = req.cookies["x-access-token"];
+    if (token) {
+      
+      const decode = jwt.verify(token, "secreat key");
+      var sql =
+        "SELECT * FROM phi WHERE  MCI=" + mysqlConnection.escape(decode.data);
+      mysqlConnection.query(
+        {
+          sql: sql,
+          timeout: 40000,
+        },
+        function (error, results, fields) {
+          if (!error) {
+            var json = JSON.parse(JSON.stringify(results));
+            var data = json[0];
+            req.user = data;
+            req.token = token;
+            next();
+          } else {
+            console.log(error);
+            res.redirect("http://localhost:3000/main/loginpage.html");
+          }
+        }
+      );
+    } else {
+      // cookie not found redirect to login
+      return res.redirect("http://localhost:3000/main/loginpage.html");
+    }
+  } catch (e) {
+    res.send("Please Login!");
+  }
+};
+
+
+module.exports=auth
     res.send('Please Login! ')
   }
 }

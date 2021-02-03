28166d0cb918cd96956fff37974076514eed7b5f
Tharushi <63853025+TharushiSuwaris@users.noreply.github.com>
2021-02-03 08:30:45 +0530

    Update index.js

--- Web application/index.js
+++ Web application/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const exphbs  = require('express-handlebars');
 const { env } = require("process");
 const bodyparser=require('body-parser')
+const cookieParser = require('cookie-parser')
 
 const app=express()
 const PORT=env.PORT!=null?env.PORT:3000
@@ -10,10 +11,12 @@ const server=http.createServer(app)
 
 const mainRoute=require('./routes/main')
 const userRoute=require('./routes/user')
-
+const profileRoute=require('./routes/profile')
+const auth=require('./middlewares/auth')
 
 app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({ extended: true})); 
+app.use(bodyparser.urlencoded({ extended: true}))
+app.use(cookieParser()) 
 
 app.engine('hbs', exphbs({
     extname:'hbs',
@@ -23,6 +26,8 @@ app.set('view engine', 'hbs');
 app.use('/public',express.static(__dirname + '/public'))
 app.use('/main',mainRoute)
 app.use('/user',userRoute)
+app.use('/profile',auth,profileRoute)
+
 
 
 app.get('/',(req,res) =>{

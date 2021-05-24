7b82885d9ea1c9853bc69715f10df0fc7cd5e183
Nimashi Uthpala <67429062+Nimashi450@users.noreply.github.com>
2021-05-24 22:32:08 +0530

    Add files via upload
    new database file

---
+++ Web application/qtracker.sql
@@ -0,0 +1,137 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: qtracker
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `person`
+--
+
+DROP TABLE IF EXISTS `person`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `person` (
+  `NIC` varchar(15) NOT NULL,
+  `FirstName` varchar(25) DEFAULT NULL,
+  `LastName` varchar(25) DEFAULT NULL,
+  `Contact` varchar(12) DEFAULT NULL,
+  `StartDate` date DEFAULT NULL,
+  `EndDate` date DEFAULT NULL,
+  `AddressLine1` varchar(30) DEFAULT NULL,
+  `AddressLine2` varchar(30) DEFAULT NULL,
+  `AddressLine3` varchar(30) DEFAULT NULL,
+  `BelongArea` varchar(25) DEFAULT NULL,
+  `DeviceId` varchar(15) DEFAULT 'NOT NULL',
+  `MCI` varchar(15) DEFAULT NULL,
+  KEY `MCI` (`MCI`),
+  CONSTRAINT `MCI` FOREIGN KEY (`MCI`) REFERENCES `phi` (`MCI`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `person`
+--
+
+LOCK TABLES `person` WRITE;
+/*!40000 ALTER TABLE `person` DISABLE KEYS */;
+/*!40000 ALTER TABLE `person` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `phi`
+--
+
+DROP TABLE IF EXISTS `phi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `phi` (
+  `MCI` varchar(15) NOT NULL,
+  `FirstName` varchar(25) DEFAULT NULL,
+  `LastName` varchar(25) DEFAULT NULL,
+  `WorkingArea` varchar(25) DEFAULT NULL,
+  `Contact` varchar(12) DEFAULT NULL,
+  `Email` varchar(25) DEFAULT 'NOT NULL',
+  `Password` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`MCI`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `phi`
+--
+
+LOCK TABLES `phi` WRITE;
+/*!40000 ALTER TABLE `phi` DISABLE KEYS */;
+INSERT INTO `phi` VALUES ('1234','aaa','bbb','nikaweratiya','567-0987-765','bmnimashi@gmail.com','12345678\0\0\0\0\0\0\0\0'),('3456','kamala','uthpala','nikaweratiya','0776854326','bmnimashi@gmail.com','09876543');
+/*!40000 ALTER TABLE `phi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `records`
+--
+
+DROP TABLE IF EXISTS `records`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `records` (
+  `nic` varchar(15) NOT NULL,
+  `Date` date NOT NULL,
+  `Temp1` decimal(3,1) DEFAULT NULL,
+  `Temp2` decimal(3,1) DEFAULT NULL,
+  PRIMARY KEY (`nic`,`Date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `records`
+--
+
+LOCK TABLES `records` WRITE;
+/*!40000 ALTER TABLE `records` DISABLE KEYS */;
+/*!40000 ALTER TABLE `records` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stores`
+--
+
+DROP TABLE IF EXISTS `stores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stores` (
+  `Mic` varchar(15) DEFAULT NULL,
+  `Nic` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stores`
+--
+
+LOCK TABLES `stores` WRITE;
+/*!40000 ALTER TABLE `stores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stores` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-24 21:39:31
